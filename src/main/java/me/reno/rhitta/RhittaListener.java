cb725e905517865f87fb9b178579cb8cac465e2d
c5kua0 <miobokopolo@gmail.com>
2026-08-17 20:56:33 +0800

    Create RhittaListener.java

---
+++ src/main/java/me/reno/rhitta/RhittaListener.java
@@ -0,0 +1,12 @@
+package me.reno.rhitta;
+
+import org.bukkit.event.Listener;
+
+public class RhittaListener implements Listener {
+
+    private final RhittaPlugin plugin;
+
+    public RhittaListener(RhittaPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
