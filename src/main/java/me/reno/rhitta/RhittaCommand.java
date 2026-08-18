083e1c8c18c5e915b3f9c9f41c620e2d1a549249
c5kua0 <miobokopolo@gmail.com>
2026-08-18 11:28:32 +0800

    Update RhittaCommand.java

--- src/main/java/me/reno/rhitta/RhittaCommand.java
+++ src/main/java/me/reno/rhitta/RhittaCommand.java
@@ -16,13 +16,16 @@ public class RhittaCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        RhittaManager manager = plugin.getRhittaManager();
+
+        // Only players can use the command
         if (!(sender instanceof Player player)) {
             sender.sendMessage(ChatColor.RED + "Only players can use this command.");
             return true;
         }
 
-        RhittaManager manager = plugin.getRhittaManager();
-
+        // /rhitta
         if (args.length == 0) {
             sendStatus(player, manager);
             return true;
@@ -31,31 +34,115 @@ public class RhittaCommand implements CommandExecutor {
         String sub = args[0].toLowerCase();
 
         switch (sub) {
+
+            // /rhitta status
             case "status" -> sendStatus(player, manager);
 
+            // /rhitta give
             case "give" -> {
-                if (!manager.isOwner(player)) {
-                    player.sendMessage(ChatColor.RED + "Only the current owner of Rhitta can do that.");
+
+                if (!manager.isAllowedOwner(player)) {
+                    player.sendMessage(
+                            ChatColor.RED + "Only _ToshiroCyMc can receive Rhitta."
+                    );
                     return true;
                 }
+
                 manager.giveRhitta(player);
-                player.sendMessage(ChatColor.GREEN + "Rhitta has been given to you.");
+
+                player.sendMessage(
+                        ChatColor.GREEN + "Rhitta has been given to you."
+                );
             }
 
-            default -> player.sendMessage(ChatColor.RED + "Usage: /rhitta [status|give]");
+            // /rhitta remove PlayerName
+            case "remove" -> {
+
+                // Only the Rhitta owner can remove it
+                if (!manager.isAllowedOwner(player)) {
+                    player.sendMessage(
+                            ChatColor.RED + "Only the Rhitta owner can use this command."
+                    );
+                    return true;
+                }
+
+                if (args.length < 2) {
+                    player.sendMessage(
+                            ChatColor.RED + "Usage: /rhitta remove PlayerName"
+                    );
+                    return true;
+                }
+
+                Player target = plugin.getServer()
+                        .getPlayerExact(args[1]);
+
+                if (target == null) {
+                    player.sendMessage(
+                            ChatColor.RED + "That player is not online."
+                    );
+                    return true;
+                }
+
+                int removed = manager.removeRhitta(target);
+
+                if (removed > 0) {
+                    player.sendMessage(
+                            ChatColor.GREEN
+                                    + "Removed "
+                                    + removed
+                                    + " Rhitta item(s) from "
+                                    + target.getName()
+                                    + "."
+                    );
+
+                    target.sendMessage(
+                            ChatColor.RED
+                                    + "Rhitta was removed from your inventory."
+                    );
+                } else {
+                    player.sendMessage(
+                            ChatColor.YELLOW
+                                    + target.getName()
+                                    + " does not have Rhitta."
+                    );
+                }
+            }
+
+            default -> player.sendMessage(
+                    ChatColor.RED
+                            + "Usage: /rhitta [status|give|remove PlayerName]"
+            );
         }
 
         return true;
     }
 
     private void sendStatus(Player player, RhittaManager manager) {
+
         boolean hasIt = manager.hasRhitta(player);
         boolean owns = manager.isOwner(player);
         int defense = manager.getDefense(player);
 
-        player.sendMessage(ChatColor.GOLD + "--- Rhitta Status ---");
-        player.sendMessage(ChatColor.YELLOW + "Holding Rhitta: " + (hasIt ? "Yes" : "No"));
-        player.sendMessage(ChatColor.YELLOW + "Owner: " + (owns ? "You" : "Someone else"));
-        player.sendMessage(ChatColor.YELLOW + "Defense stacks: " + defense);
+        player.sendMessage(
+                ChatColor.GOLD + "--- Rhitta Status ---"
+        );
+
+        player.sendMessage(
+                ChatColor.YELLOW
+                        + "Holding Rhitta: "
+                        + (hasIt ? "Yes" : "No")
+        );
+
+        player.sendMessage(
+                ChatColor.YELLOW
+                        + "Owner: "
+                        + (owns ? "You" : "Someone else")
+        );
+
+        player.sendMessage(
+                ChatColor.YELLOW
+                        + "Defense stacks: "
+                        + defense
+        );
     }
-}
+            }
