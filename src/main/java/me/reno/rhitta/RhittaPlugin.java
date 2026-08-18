20f79b960c205b90da95c9277e6e550e643b53b0
c5kua0 <miobokopolo@gmail.com>
2026-08-18 08:45:52 +0800

    Update RhittaPlugin.java

--- src/main/java/me/reno/rhitta/RhittaPlugin.java
+++ src/main/java/me/reno/rhitta/RhittaPlugin.java
@@ -4,13 +4,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RhittaPlugin extends JavaPlugin {
 
+    private RhittaManager rhittaManager;
+
     @Override
     public void onEnable() {
+        rhittaManager = new RhittaManager(this);
+
+        getServer().getPluginManager().registerEvents(
+                new RhittaListener(this),
+                this
+        );
+
+        getCommand("rhitta").setExecutor(new RhittaCommand(this));
+
         getLogger().info("Rhitta has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Rhitta has been disabled!");
+    }
+
+    public RhittaManager getRhittaManager() {
+        return rhittaManager;
     }
 }
