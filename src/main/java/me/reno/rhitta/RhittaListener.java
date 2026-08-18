a383fd9b642efc9a586681eca2288aca43e5a503
c5kua0 <miobokopolo@gmail.com>
2026-08-18 09:38:41 +0800

    Update RhittaListener.java

--- src/main/java/me/reno/rhitta/RhittaListener.java
+++ src/main/java/me/reno/rhitta/RhittaListener.java
@@ -1,7 +1,9 @@
 package me.reno.rhitta;
 
+import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,15 +12,13 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class RhittaListener implements Listener {
 
-    private final RhittaPlugin plugin;
+    private final RhittaManager manager;
 
-    public RhittaListener(RhittaPlugin plugin) {
-        this.plugin = plugin;
+    public RhittaListener(RhittaManager manager) {
+        this.manager = manager;
     }
 
     @EventHandler
@@ -28,81 +28,108 @@ public class RhittaListener implements Listener {
             return;
         }
 
-        if (!(event.getEntity() instanceof LivingEntity)) {
+        Entity entity = event.getEntity();
+
+        if (!(entity instanceof LivingEntity target)) {
             return;
         }
 
-        RhittaManager manager = plugin.getRhittaManager();
+        ItemStack item = player.getInventory().getItemInMainHand();
 
-        if (!manager.isOwner(player)) {
+        if (!manager.isRhitta(item)) {
             return;
         }
 
-        ItemStack weapon = player.getInventory().getItemInMainHand();
+        // Life steal
+        double damage = event.getFinalDamage();
+        double heal = Math.min(damage * 0.25, 4.0);
 
-        if (!manager.isRhitta(weapon)) {
-            return;
+        double newHealth = Math.min(
+                player.getHealth() + heal,
+                getMaxHealth(player)
+        );
+
+        player.setHealth(newHealth);
+
+        // Defense growth
+        manager.addDefense(player);
+
+        // Prevent Rhitta from losing durability
+        if (item.getType().getMaxDurability() > 0) {
+            item.setDurability((short) 0);
         }
-
-        double heal = Math.min(event.getFinalDamage() * 0.35, 8.0);
-
-        AttributeInstance maxHealth =
-                player.getAttribute(Attribute.MAX_HEALTH);
-
-        if (maxHealth != null) {
-            player.setHealth(
-                    Math.min(
-                            player.getHealth() + heal,
-                            maxHealth.getValue()
-                    )
-            );
-        }
-
-        manager.addDefense(player, 1);
-        manager.repairRhitta(weapon, 1);
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
 
         Player player = event.getEntity();
-        RhittaManager manager = plugin.getRhittaManager();
 
         if (!manager.isOwner(player)) {
             return;
         }
 
-        if (!manager.hasResurrection(player)) {
-            return;
+        ItemStack rhitta = null;
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (manager.isRhitta(item)) {
+                rhitta = item;
+                break;
+            }
         }
 
-        manager.prepareResurrection(player);
+        // Keep Rhitta from dropping
+        if (rhitta != null) {
+            event.getDrops().removeIf(manager::isRhitta);
+        }
 
-        event.setKeepInventory(true);
-        event.getDrops().removeIf(manager::isRhitta);
+        // One-time resurrection
+        if (manager.canResurrect(player)) {
+            event.setKeepInventory(true);
+            event.getDrops().clear();
+
+            manager.prepareResurrection(player);
+        }
     }
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
 
         Player player = event.getPlayer();
-        RhittaManager manager = plugin.getRhittaManager();
-
-        if (!manager.isOwner(player)) {
-            return;
-        }
 
         if (!manager.isResurrectionPending(player)) {
             return;
         }
 
-        manager.completeResurrection(player);
+        manager.resurrect(player);
 
-        // 200% Strength for 1 minute
-        player.addPotionEffect(
-                new PotionEffect(
-                        PotionEffectType.STRENGTH,
-                        20 * 60,
+        player.sendMessage(
+                ChatColor.GOLD + "✦ RHITTA HAS CHOSEN YOU ✦"
+        );
+
+        player.sendMessage(
+                ChatColor.RED + "Resurrection activated!"
+        );
+
+        player.sendMessage(
+                ChatColor.RED + "+200% Strength"
+                + ChatColor.GRAY + " | "
+                + ChatColor.BLUE + "+200% Defense"
+        );
+    }
+
+    private double getMaxHealth(Player player) {
+
+        AttributeInstance attribute =
+                player.getAttribute(Attribute.MAX_HEALTH);
+
+        if (attribute == null) {
+            return 20.0;
+        }
+
+        return attribute.getValue();
+    }
+            }                        20 * 60,
                         2,
                         false,
                         true,
