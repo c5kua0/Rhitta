d4fbf5234397f97e0c68ce06a3740172955752a5
c5kua0 <miobokopolo@gmail.com>
2026-08-18 15:12:19 +0800

    Update RhittaListener.java

--- src/main/java/me/reno/rhitta/RhittaListener.java
+++ src/main/java/me/reno/rhitta/RhittaListener.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.action.Action;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
