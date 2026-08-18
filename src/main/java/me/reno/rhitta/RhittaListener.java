ee938d305e34835ab8a71459f778747fb36cfc7d
c5kua0 <miobokopolo@gmail.com>
2026-08-18 09:33:50 +0800

    Update RhittaListener.java

--- src/main/java/me/reno/rhitta/RhittaListener.java
+++ src/main/java/me/reno/rhitta/RhittaListener.java
@@ -1,7 +1,7 @@
 package me.reno.rhitta;
 
-import org.bukkit.Attribute;
-import org.bukkit.EntityEffect;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,7 +28,7 @@ public class RhittaListener implements Listener {
             return;
         }
 
-        if (!(event.getEntity() instanceof LivingEntity target)) {
+        if (!(event.getEntity() instanceof LivingEntity)) {
             return;
         }
 
@@ -44,35 +44,28 @@ public class RhittaListener implements Listener {
             return;
         }
 
-        // LIFE STEAL
         double heal = Math.min(event.getFinalDamage() * 0.35, 8.0);
 
-        double maxHealth = player.getAttribute(
-                Attribute.MAX_HEALTH
-        ).getValue();
+        AttributeInstance maxHealth =
+                player.getAttribute(Attribute.MAX_HEALTH);
 
-        double newHealth = Math.min(
-                player.getHealth() + heal,
-                maxHealth
-        );
+        if (maxHealth != null) {
+            player.setHealth(
+                    Math.min(
+                            player.getHealth() + heal,
+                            maxHealth.getValue()
+                    )
+            );
+        }
 
-        player.setHealth(newHealth);
-
-        // DEFENSE GROWTH
         manager.addDefense(player, 1);
-
-        // DURABILITY REPAIR
         manager.repairRhitta(weapon, 1);
-
-        // HIT EFFECT
-        target.playEffect(EntityEffect.HURT);
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
 
         Player player = event.getEntity();
-
         RhittaManager manager = plugin.getRhittaManager();
 
         if (!manager.isOwner(player)) {
@@ -85,20 +78,51 @@ public class RhittaListener implements Listener {
 
         manager.prepareResurrection(player);
 
-        // REMOVE RHITTA FROM DROPS
-        event.getDrops().removeIf(manager::isRhitta);
-
         event.setKeepInventory(true);
-        event.getDrops().clear();
+        event.getDrops().removeIf(manager::isRhitta);
     }
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
 
         Player player = event.getPlayer();
-
         RhittaManager manager = plugin.getRhittaManager();
 
+        if (!manager.isOwner(player)) {
+            return;
+        }
+
+        if (!manager.isResurrectionPending(player)) {
+            return;
+        }
+
+        manager.completeResurrection(player);
+
+        // 200% Strength for 1 minute
+        player.addPotionEffect(
+                new PotionEffect(
+                        PotionEffectType.STRENGTH,
+                        20 * 60,
+                        2,
+                        false,
+                        true,
+                        true
+                )
+        );
+
+        // 200% Defense for 1 minute
+        player.addPotionEffect(
+                new PotionEffect(
+                        PotionEffectType.RESISTANCE,
+                        20 * 60,
+                        2,
+                        false,
+                        true,
+                        true
+                )
+        );
+    }
+    }
         if (!manager.isOwner(player)) {
             return;
         }
