32463ca6db35aaf97d3e805e621cbccfc2eff6be
c5kua0 <miobokopolo@gmail.com>
2026-08-20 18:16:38 +0800

    Update RhittaCommand.java

--- src/main/java/me/reno/rhitta/RhittaCommand.java
+++ src/main/java/me/reno/rhitta/RhittaCommand.java
@@ -6,55 +6,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class RhittaCommand
-        implements CommandExecutor {
+public class RhittaCommand implements CommandExecutor {
 
     private final RhittaPlugin plugin;
 
-    public RhittaCommand(
-            RhittaPlugin plugin) {
-
+    public RhittaCommand(RhittaPlugin plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(
-            CommandSender sender,
-            Command command,
-            String label,
-            String[] args) {
-
-        if (!(sender instanceof Player player)) {
-
-            sender.sendMessage(
-                    ChatColor.RED
-                            + "Only players can use this command."
-            );
-
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
             return true;
         }
 
-        if (!player.getName()
-                .equalsIgnoreCase(
-                        "_ToshiroCyMc"
-                )) {
+        Player player = (Player) sender;
 
-            player.sendMessage(
-                    ChatColor.RED
-                            + "You are not the owner of Rhitta."
-            );
-
+        if (!plugin.getRhittaManager().isAllowedOwner(player)) {
+            sender.sendMessage(ChatColor.RED + "You are not the owner of Rhitta.");
             return true;
         }
 
-        plugin.getRhittaManager()
-                .giveRhitta(player);
-
-        player.sendMessage(
-                ChatColor.GOLD
-                        + "Rhitta restored!"
-        );
-
+        plugin.getRhittaManager().giveRhitta(player);
+        sender.sendMessage(ChatColor.GOLD + "Rhitta restored!");
         return true;
     }
 }
